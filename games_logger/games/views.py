91b3a9e6808a58463cfb1c955c98481db876a3a4
HaeckelK <haeckelk.github@gmail.com>
2021-01-07 08:44:09 +0000

    feat: admin access to Player, Game, Categories

--- games_logger/games/views.py
+++ games_logger/games/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def hello_world(request):
     return render(request, 'hello_world.html', {})
+
+
+def index(request):
+    """Landing page"""
+    return render(request, "index.html", {})
