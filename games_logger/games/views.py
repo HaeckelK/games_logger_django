bc189d357fa311e03292c85f8aa106db55faa30e
HaeckelK <51128712+HaeckelK@users.noreply.github.com>
2021-01-09 16:42:28 +0000

    feat: basic player page (#3)
    Co-authored-by: HaeckelK <haeckelk.github@gmail.com>

--- games_logger/games/views.py
+++ games_logger/games/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Match
+from .models import Match, Player
 
 
 def hello_world(request):
@@ -11,3 +11,13 @@ def index(request):
     n = int(request.GET.get("n", 5))
     context = {"matches": Match.objects.order_by('-created_on')[:n]}
     return render(request, "index.html", context)
+
+
+def player_detail(request, pk: int):
+    """Player information page."""
+    played = Match.objects.filter(players__in=[pk]).count()
+    won = Match.objects.filter(winners__in=[pk]).count()
+    context = {"player": Player.objects.get(pk=pk),
+               "played": played,
+               "won": won}
+    return render(request, "player.html", context)
